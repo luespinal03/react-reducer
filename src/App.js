a79f1482b3ce07c0ddd119c1c02fe103b489f584
Luis Espinal <luisespinal294@gmail.com>
2023-03-27 12:38:47 -0400

    refactored switch case

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
+import React, { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0)
+  const [inputHandler, setInputHandler] = useState(0)
+
+  const counterFuncAdd = () => {
+    setCounter(counter + 1)
+  }
+
+  const counterFuncSub = () => {
+    setCounter(counter - 1)
+  }
+
+  const counterFuncParam = (type) => {
+    type === 'add' && setCounter(counter + 1)
+    type === 'sub' && setCounter(counter - 1)
+    type === 'divide' && setCounter(counter / 2)
+    type === 'multiply' && setCounter(counter * 2)
+  }
+  // const counterFuncInput = (type) => {
+  //   type === 'add' && setCounter(counter+inputHandler)
+  //   type === 'sub' && setCounter(counter-inputHandler)
+  //   type === 'divide' && setCounter(counter / inputHandler)
+  //   type === 'multiply' && setCounter(counter * inputHandler)
+  // }
+  const counterFuncInput = (type) => {
+    switch (type) {
+      case 'add':
+        setCounter(counter + inputHandler)
+        break;
+      case 'sub':
+        setCounter(counter - inputHandler)
+        break;
+      case 'divide':
+        setCounter(counter / inputHandler)
+        break;
+      case 'multiply':
+        setCounter(counter * inputHandler)
+        break;
+      default:
+        alert("Not a Math function")
+        break;
+    }
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App App-header">
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => setCounter(0)}>Reset</button>
+      {/* <button onClick={counterFuncAdd}>Add 1</button>
+      <button onClick={counterFuncSub}>Subtract 1</button> */}
+      <button onClick={() => counterFuncParam('add')}>Add 1</button>
+      <button onClick={() => counterFuncParam('sub')}>Subtract 1</button>
+      <button onClick={() => counterFuncParam('divide')}>Divide by 2</button>
+      <button onClick={() => counterFuncParam('multiply')}>Multiply by 2</button>
+      <p>--------------------------------------------------------------------</p>
+      <input type='number' onChange={(e) => setInputHandler(parseInt(e.target.value))}></input>
+      <button onClick={() => counterFuncInput('add')}>Add {inputHandler}</button>
+      <button onClick={() => counterFuncInput('sub')}>Subtract {inputHandler}</button>
+      <button onClick={() => counterFuncInput('divide')}>Divide by {inputHandler}</button>
+      <button onClick={() => counterFuncInput('multiply')}>Multiply by {inputHandler}</button>
     </div>
   );
 }
